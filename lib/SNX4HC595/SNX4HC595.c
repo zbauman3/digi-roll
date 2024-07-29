b34c7546f2ec8111f39fa7e67f0377692c8a3da5
Zane Bauman <zanebauman55@gmail.com>
2024-07-29 08:33:12 -0400

    init

---
+++ lib/SNX4HC595/SNX4HC595.c
@@ -0,0 +1,52 @@
+#ifndef _ZANE_SNX4HC595_
+#define _ZANE_SNX4HC595_
+
+#include <Arduino.h>
+
+typedef struct {
+  uint8_t sclk;
+  uint8_t rclk;
+  uint8_t data;
+} SNX4HC595Config;
+
+void SNX4HC595_sendData(SNX4HC595Config *config) {
+  digitalWrite(config->sclk, HIGH);
+  digitalWrite(config->sclk, LOW);
+}
+
+void SNX4HC595_showData(SNX4HC595Config *config) {
+  digitalWrite(config->rclk, HIGH);
+  digitalWrite(config->rclk, LOW);
+}
+
+void SNX4HC595_sendByte(SNX4HC595Config *config, uint8_t value) {
+  uint8_t mask;
+  int8_t i;
+
+  for (i = 7; i >= 0; i--) {
+    mask = value & (1 << i);
+    if (mask == 0) {
+      digitalWrite(config->data, LOW);
+    } else {
+      digitalWrite(config->data, HIGH);
+    }
+    SNX4HC595_sendData(config);
+  }
+  SNX4HC595_showData(config);
+}
+
+void SNX4HC595_clear(SNX4HC595Config *config) { SNX4HC595_sendByte(config, 0); }
+
+void SNX4HC595_setup(SNX4HC595Config *config) {
+  pinMode(config->sclk, OUTPUT);
+  pinMode(config->rclk, OUTPUT);
+  pinMode(config->data, OUTPUT);
+
+  digitalWrite(config->sclk, LOW);
+  digitalWrite(config->rclk, LOW);
+  digitalWrite(config->data, LOW);
+
+  SNX4HC595_clear(config);
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,137 @@
+#include "Adafruit_LEDBackpack.h"
+#include <Adafruit_GFX.h>
+#include <Arduino.h>
+#include <SNX4HC595.c>
+#include <Wire.h>
+
+// ATMEL ATTINY84 / ARDUINO
+//
+//                           +-\/-+
+//                     VCC  1|    |14  GND
+//             (D  0)  PB0  2|    |13  PA0  (D 10)        AREF
+//             (D  1)  PB1  3|    |12  PA1  (D  9)
+//             (D 11)  PB3  4|    |11  PA2  (D  8)
+//  PWM  INT0  (D  2)  PB2  5|    |10  PA3  (D  7)
+//  PWM        (D  3)  PA7  6|    |9   PA4  (D  6)
+//  PWM        (D  4)  PA6  7|    |8   PA5  (D  5)        PWM
+//
+
+#define BTN_A0 0
+#define BTN_A1 1
+#define BTN_A2 2
+#define SHIFT_CLK 10
+#define SHIFT_REG 8
+#define SHIFT_DAT 9
+
+Adafruit_7segment matrix = Adafruit_7segment();
+
+SNX4HC595Config config595 = {
+    .sclk = SHIFT_CLK,
+    .rclk = SHIFT_REG,
+    .data = SHIFT_DAT,
+};
+
+void setup() {
+  matrix.begin(0x70);
+  SNX4HC595_setup(&config595);
+  pinMode(BTN_A0, INPUT_PULLUP);
+  pinMode(BTN_A1, INPUT_PULLUP);
+  pinMode(BTN_A2, INPUT_PULLUP);
+
+  matrix.print(0xBEBF, HEX);
+  matrix.writeDisplay();
+  matrix.setBrightness(7);
+  // matrix.writeDigitRaw()
+}
+
+void loop() {
+  bool btn0 = digitalRead(BTN_A0) == LOW;
+  bool btn1 = digitalRead(BTN_A1) == LOW;
+  bool btn2 = digitalRead(BTN_A2) == LOW;
+
+  if (btn0 || btn1 || btn2) {
+    uint8_t data = 0;
+
+    if (btn2) {
+      if (btn1) {
+        if (btn0) {
+          data = 0b00000001;
+        } else {
+          data = 0b00000010;
+        }
+      } else if (btn0) {
+        data = 0b00000100;
+      } else {
+        data = 0b00001000;
+      }
+    } else if (btn1) {
+      if (btn0) {
+        data = 0b00010000;
+      } else {
+        data = 0b00100000;
+      }
+    } else {
+      data = 0b01000000;
+    }
+    SNX4HC595_sendByte(&config595, data);
+  }
+  delay(10);
+
+  // SNX4HC595_sendByte(&config595, 0b01000000);
+  // delay(1000);
+  // SNX4HC595_sendByte(&config595, 0b00000000);
+  // delay(1000);
+  // SNX4HC595_sendByte(&config595, 0b00000001);
+  // delay(1000);
+  // SNX4HC595_sendByte(&config595, 0b00000000);
+  // delay(1000);
+
+  // // try to print a number thats too long
+  // matrix.print(10000, DEC);
+  // matrix.writeDisplay();
+  // delay(1000);
+
+  // // print a hex number
+  // matrix.print(0xBEEF, HEX);
+  // matrix.writeDisplay();
+  // delay(1000);
+
+  // // print a floating point
+  // matrix.print(12.34);
+  // matrix.writeDisplay();
+  // delay(1000);
+
+  // // print a string message
+  // matrix.print("7SEG");
+  // matrix.writeDisplay();
+  // delay(10000);
+
+  // // print with print/println
+  // for (uint16_t counter = 0; counter < 9999; counter++) {
+  //   matrix.println(counter);
+  //   matrix.writeDisplay();
+  //   delay(10);
+  // }
+
+  // // method #2 - draw each digit
+  // uint16_t blinkcounter = 0;
+  // boolean drawDots = false;
+  // for (uint16_t counter = 0; counter < 9999; counter++) {
+  //   matrix.writeDigitNum(0, (counter / 1000), drawDots);
+  //   matrix.writeDigitNum(1, (counter / 100) % 10, drawDots);
+  //   matrix.drawColon(drawDots);
+  //   matrix.writeDigitNum(3, (counter / 10) % 10, drawDots);
+  //   matrix.writeDigitNum(4, counter % 10, drawDots);
+
+  //   blinkcounter += 50;
+  //   if (blinkcounter < 500) {
+  //     drawDots = false;
+  //   } else if (blinkcounter < 1000) {
+  //     drawDots = true;
+  //   } else {
+  //     blinkcounter = 0;
+  //   }
+  //   matrix.writeDisplay();
+  //   delay(10);
+  // }
+}
